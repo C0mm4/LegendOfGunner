2e288abee783d7b51e2370bb9e91938312f2ee27
C0mm4 <comonhuman@gmail.com>
2025-07-29 14:55:10 +0900

    Chore : Add Foldering

---
+++ Assets/Scripts/Weapon/BaseWeaponHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeaponHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float delay = 0.2f;
+    public float Delay {  get { return delay; } set { delay = value; } }
+
+    [SerializeField]
+    private float power = 1f;
+    public float Power { get { return power; } set { power = value; } }
+
+    [SerializeField]
+    public float speed = 1f;
+    public float Speed {  get { return speed; } set { speed = value; } }
+
+    [SerializeField]
+    private float attackRange = 10f;
+    [SerializeField]
+    public float AttackRange {  get { return attackRange; } set { attackRange = value; } }
+
+
+    public LayerMask target;
+
+    private static readonly int IsAttack = Animator.StringToHash("IsAttack");
+    private static readonly int IsReload = Animator.StringToHash("IsReload");
+}
