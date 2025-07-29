f04fb92c4e8c0e7c44ceb7115c75d2b686c92bcd
taewooo513 <taewooo030513@gmail.com>
2025-07-29 21:23:08 +0900

    feat: EnemyCore

---
+++ Assets/Scripts/Enemy/Core/EnemyFactory.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static EnemyManager;
+
+public class EnemyFactory : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tempObject;
+    public GameObject AddEnemy(EnemyManager.eEnemyType enemyType, Vector3 spawnPos)
+    {
+        GameObject obj = null;
+        if (tempObject.TryGetComponent(out EnemyController component))
+        {
+            Debug.Log("Is not Enemy!" + tempObject);
+            return null;
+        }
+
+        switch (enemyType)
+        {
+            case EnemyManager.eEnemyType.eTemp:
+                obj = Instantiate(tempObject, spawnPos, Quaternion.identity);
+                obj.GetComponent<EnemyModel>().SettingStatus(1, 1, 1);
+                break;
+        }
+
+        return obj;
+    }
+}
---
+++ Assets/Scripts/Enemy/Core/EnemyManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoSingleton<EnemyManager>
+{
+    public enum eEnemyType
+    {
+        eTemp,
+        eEnd,
+    }
+    [SerializeField]
+    private EnemyFactory enemyFactory;
+    private List<GameObject> enemyList;
+
+    public void Start()
+    {
+        enemyList = new List<GameObject>();
+    }
+    public void AddEnemy(eEnemyType idx, Vector3 spawnPos)
+    {
+        if (enemyFactory == null)
+        {
+            Debug.Log("Null EnemyFactory");
+        }
+        var obj = enemyFactory.AddEnemy(idx, spawnPos);
+        if (obj != null)
+        {
+            enemyList.Add(obj);
+        }
+    }
+    public void RemoveObject(GameObject obj)
+    {
+        enemyList.Remove(obj);
+    }
+    public int GetEnemyListSize()
+    {
+        return enemyList.Count;
+    }
+}
