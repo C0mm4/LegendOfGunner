cf9ceeb654a7f63bebe98020412d5448ff830a46
KO <rokid_@naver.com>
2025-07-30 11:46:45 +0900

    Feat : 메인 씬 추가

---
+++ Assets/Scripts/UI/MainSceneUI/GameStartBtn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartBtn : MonoBehaviour
+{
+    public Button startBtn;
+
+    public void StartButtonClick()
+    {
+        Debug.Log("게임 시작");
+    }
+}
