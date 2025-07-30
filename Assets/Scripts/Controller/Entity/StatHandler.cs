35ce803aea0484a79136f17e77398adfbc0f3426
C0mm4 <comonhuman@gmail.com>
2025-07-30 14:24:42 +0900

    Fix : StatHandler 값 변화 추가

--- Assets/Scripts/Controller/Entity/StatHandler.cs
+++ Assets/Scripts/Controller/Entity/StatHandler.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class StatHandler : MonoBehaviour
 {
-    [Range(1, 100)][SerializeField] private int health = 10;
-    public int Health
+    [Range(0, 100)][SerializeField] private float health = 10;
+    public float Health
     {
         get => health;
         set => health = Mathf.Clamp(value, 0, 100);
