abb43faa5528356b1ee3121e94bea51e04b8c8c6
smjawhn <84219257+smjawhn@users.noreply.github.com>
2025-07-31 19:10:41 +0900

    인게임씬 구성물 추가

--- Assets/Scripts/Controller/Entity/StatHandler.cs
+++ Assets/Scripts/Controller/Entity/StatHandler.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class StatHandler : MonoBehaviour
 {
-    [Range(0, 100)][SerializeField] private float health = 10;
+    private float health = 10;
     public float Health
     {
         get => health;
         set => health = Mathf.Clamp(value, 0, 100);
     }
 
-    [Range(1f, 20f)][SerializeField] private float speed = 3;
+    private float speed = 3;
     public float Speed
     {
         get => speed;
         set => speed = Mathf.Clamp(value, 0, 20);
     }
+
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        set => level = Mathf.Clamp(value, 0, 20);
+    }
+
         
 }
