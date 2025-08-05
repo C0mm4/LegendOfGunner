e4d8fc70f7105d19a6aac4b89b63b74c06f8ab8b
smjawhn <84219257+smjawhn@users.noreply.github.com>
2025-08-05 09:14:38 +0900

    특성 ui 연결 1차

--- Assets/Scripts/UI/GameUI/DungeonUI/GameUI/StatusUI/StatusUIManager.cs
+++ Assets/Scripts/UI/GameUI/DungeonUI/GameUI/StatusUI/StatusUIManager.cs
@@ -19,6 +19,21 @@ public class StatusUIManager : MonoBehaviour
     public TMP_Text AR_granade;
     public TMP_Text AR_laser;
     public TMP_Text AR_CD;
+    public TMP_Text SG_AddAngle;
+    public TMP_Text SG_AtkSpd;
+    public TMP_Text SG_knockback;
+    public TMP_Text SG_Piercing;
+    public TMP_Text SG_CD;
+    public TMP_Text SR_Dmg;
+    public TMP_Text SR_HeadShot;
+    public TMP_Text SR_Laser;
+    public TMP_Text SR_Piercing;
+    public TMP_Text SR_CD;
+
+
+
+
+
 
 
     ResourceController resourceController;
@@ -43,13 +58,24 @@ public class StatusUIManager : MonoBehaviour
         HG_double.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.HG, 2);
         HG_laser.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.HG, 3);
         HG_triple.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.HG, 4);
-        /*
-        // AR
-        AR_AtkSpd.text = attributeManager.GetAttributeLevelString(WeaponType.AR, 0);
-        AR_AddBullet.text = attributeManager.GetAttributeLevelString(WeaponType.AR, 1);
-        AR_granade.text = attributeManager.GetAttributeLevelString(WeaponType.AR, 2);
-        AR_laser.text = attributeManager.GetAttributeLevelString(WeaponType.AR, 3);
-        AR_CD.text = attributeManager.GetAttributeLevelString(WeaponType.AR, 4);
-        */
+
+
+        AR_AtkSpd.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.AR, 0);
+        AR_AddBullet.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.AR, 1);
+        AR_granade.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.AR, 2);
+        AR_laser.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.AR, 3);
+        AR_CD.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.AR, 4);
+
+        SG_AddAngle.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SG, 0);
+        SG_AtkSpd.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SG, 1);
+        SG_knockback.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SG, 2);
+        SG_Piercing.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SG, 3);
+        SG_CD.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SG, 4);
+
+        SR_Dmg.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SR, 0);
+        SR_HeadShot.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SR, 1);
+        SR_Laser.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SR, 2);
+        SR_Piercing.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SR, 3);
+        SR_CD.text = attributeManager.GetAttributeLevelString(AttributeManager.WeaponType.SR, 4);
     }
 }
